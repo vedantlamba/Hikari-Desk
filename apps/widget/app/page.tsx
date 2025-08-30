fa5ef2297ef7f0f29f1821f8a8663cdae7c4bef5
Vedant <vedantlamba18@gmail.com>
2025-08-30 21:49:09 +0530

    i am done with the setup, now its time to setup conex

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,13 @@
-import { add } from "@workspace/math/add";
 import { Button } from "@workspace/ui/components/button";
+
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
+        <h1 className="text-2xl font-bold">Hello Widget</h1>
         <Button size="sm">Button</Button>
-        <p>{add(2, 2)}</p>
+       
       </div>
     </div>
   );
