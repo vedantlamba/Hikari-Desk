44c5a17c7c3f6045698257e025d01b9594fdebe3
Vedant <vedantlamba18@gmail.com>
2025-08-31 17:39:46 +0530

    Finally done with convex setup, it was tough and i need to be familiar with it more

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,16 @@
+"use client";
+import { add } from "@workspace/math/add";
 import { Button } from "@workspace/ui/components/button";
-
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Widget</h1>
-        <Button size="sm">Button</Button>
-       
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <div>app/widget</div>
+      <div className="mx-auto text-balance">
+        {JSON.stringify(users, null, 2)}
       </div>
     </div>
   );
---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,9 @@
+import { query } from "./_generated/server";
+
+export const getMany = query({
+  args: {},
+  handler: async (ctx) => {
+    const users = await ctx.db.query("users").collect();
+    return users;
+  },
+});
